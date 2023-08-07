232fa6f8e62d74fdf0e49b3202dbccb94a3abee6
Piyush Prabhakar Kanadje <41730532+piyushkanadje@users.noreply.github.com>
2023-08-07 13:01:21 +0530

    Time: 45 ms (96.50%) | Memory: 16.7 MB (99.39%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -1,10 +1,16 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        # there are 3 different way we can solve this problem 
+        #1-brute fouce with 2 for loop
+        #2- applying binary search to 2d matrix
+        #3- by converting 2d matrix into 1d
+
+        #2
+
         row  = 0
         col = len(matrix[row]) - 1
 
         while  row  < len(matrix) and col >=0:
-
             if matrix[row][col] == target:
                 return True
             
@@ -12,8 +18,17 @@ class Solution:
                 row +=1
             else:
                 col -=1
-            
         return False
+
+        # #3
+
+        # row = len(matrix)
+        # col = len(matrix[0])
+        # l , h = 0 , row*col -1
+
+        # while l < h:
+
+
 
         
  
