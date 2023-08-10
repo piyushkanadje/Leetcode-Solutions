3ff89a0bf5766f8b1fe22655841995ccfa766296
Piyush Prabhakar Kanadje <41730532+piyushkanadje@users.noreply.github.com>
2023-08-10 22:13:57 +0530

    Time: 58 ms (87.04%) | Memory: 17 MB (47.39%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.py
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.py
@@ -12,14 +12,13 @@ class Solution:
                 continue
             
             if nums[low] <= nums[mid]:
-                if nums[low] <= target <= nums[mid]:
-                    high = mid - 1
+                if  target > nums[mid]  or target < nums[low]:
+                    low = mid  + 1
                 else:
-                    low = mid + 1
+                    high = mid - 1
             else:
-                if nums[mid] <= target <= nums[high]:
-                    low = mid + 1
+                if target < nums[mid] or target > nums[high] :
+                    high = mid -  1
                 else:
-                    high = mid - 1
-        
+                    low = mid  + 1
         return False
