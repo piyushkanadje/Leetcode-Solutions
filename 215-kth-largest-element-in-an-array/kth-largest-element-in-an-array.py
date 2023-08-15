fe75445ec5be27e4eef4c979163ddb499679365e
Piyush Prabhakar Kanadje <41730532+piyushkanadje@users.noreply.github.com>
2023-08-15 16:36:52 +0530

    Time: 1494 ms (13.65%) | Memory: 32.7 MB (16.37%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
@@ -1,28 +1,27 @@
 import random
+
 class Solution:
-    def randomizedPartition(self,arr, low, high):
-        pivotIndex = random.randint(low,high)
-        arr[pivotIndex], arr[high]= arr[high], arr[pivotIndex]
-        i = low -1
-        pivot = arr[high]
 
+    def partition(self, arr, low, high):
+        x = arr[high]
+        i = low
         for j in range(low, high):
-            if arr[j]>=pivot:
-                i+=1
-                arr[i], arr[j]= arr[j], arr[i]
-        arr[i+1], arr[high]= arr[high], arr[i+1]
-        return i+1
+            if arr[j] <= x:
+                arr[j], arr[i] = arr[i], arr[j]
+                i += 1
+        arr[i], arr[high] = arr[high], arr[i]
+        return i 
 
-    def randomizedSelection(self,arr, low, high, k):
-        if low== high:
-            return arr[low]
-        pivotIndex = self.randomizedPartition(arr,low, high)
-        if k == pivotIndex:
-            return arr[pivotIndex]
-        elif k<=pivotIndex:
-            return self.randomizedSelection(arr,low, pivotIndex-1, k)
-        else:
-            return self.randomizedSelection(arr, pivotIndex+1, high, k)
-        
-    def findKthLargest(self, nums: List[int], k: int) -> int:
-        return self.randomizedSelection(nums,0,len(nums)-1,k-1)
+    def kthSmallest(self, arr, low, high, k):
+        if k > 0 and k <= high - low + 1:
+            index = self.partition(arr, low, high)
+            if index - low == k - 1:
+                return arr[index]
+            if index - low > k - 1:
+                return self.kthSmallest(arr, low, index - 1, k)
+            return self.kthSmallest(arr, index + 1, high, k - index + low - 1)
+
+    def findKthLargest(self, nums, k):
+        return self.kthSmallest(nums, 0, len(nums) - 1, len(nums) - k + 1)
+
+
