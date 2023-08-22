a544edd0ddca346a49c6e4275a77bbe4aac512c2
Piyush Prabhakar Kanadje <41730532+piyushkanadje@users.noreply.github.com>
2023-08-22 14:15:09 +0530

    Time: 110 ms (69.62%) | Memory: 17.7 MB (24.42%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.py
+++ 572-subtree-of-another-tree/subtree-of-another-tree.py
@@ -1,27 +1,4 @@
-# # Definition for a binary tree node.
-# # class TreeNode:
-# #     def __init__(self, val=0, left=None, right=None):
-# #         self.val = val
-# #         self.left = left
-# #         self.right = right
-# class Solution:
-#     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-
-        
-#         def sameTree(root, subRoot):
-#             if not root and not subRoot:
-#                 return True
-#             elif (root and subRoot):
-#                 return (root.val == subRoot.val) and sameTree(root.left,subRoot.left) and sameTree(root.right, subRoot.right)
-#             else:
-#                 return False
-        
-#         return bool(root and subRoot) and  ( sameTree(root, subRoot) or sameTree(root.left, subRoot) or sameTree(root.right, subRoot) )
-    
-
-
-
-    # Definition for a binary tree node.
+# Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
@@ -29,14 +6,15 @@
 #         self.right = right
 class Solution:
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        
-        def isSame(root, subRoot):
 
+        
+        def sameTree(root, subRoot):
             if not root and not subRoot:
                 return True
-            elif root and subRoot:
-               return (root.val == subRoot.val) and isSame(root.right,subRoot.right) and isSame(root.left, subRoot.left)
+            elif (root and subRoot):
+                return (root.val == subRoot.val) and sameTree(root.left,subRoot.left) and sameTree(root.right, subRoot.right)
             else:
                 return False
         
-        return bool(root and subRoot) and( isSame(root, subRoot) or self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot) )
+        return bool(root and subRoot) and  ( sameTree(root, subRoot) or self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot) )
+    
