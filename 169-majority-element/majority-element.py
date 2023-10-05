56979008fb83f2713c306c08283876bbdb16db5f
Piyush Prabhakar Kanadje <41730532+piyushkanadje@users.noreply.github.com>
2023-10-05 07:15:27 +0530

    Time: 149 ms (66.20%) | Memory: 17.8 MB (53.06%) - LeetSync

---
+++ 169-majority-element/majority-element.py
@@ -0,0 +1,10 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        dict = {}
+        for i in nums:
+            dict[i] = 1 + dict.get(i,0)
+        
+        for i in dict:
+            if dict.get(i) > len(nums)/2:
+                return i
+        
