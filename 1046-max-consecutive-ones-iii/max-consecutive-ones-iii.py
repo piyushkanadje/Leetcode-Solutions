1f3ad4cd4b1d24815fec674f27c71c1d913f9486
Piyush Prabhakar Kanadje <41730532+piyushkanadje@users.noreply.github.com>
2024-04-08 14:31:07 -0700

    Time: 447 ms (45.86%) | Memory: 17 MB (93.61%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
@@ -4,9 +4,10 @@ class Solution:
         left = 0
         ans= 0
         for right in range(len(nums)):
+            #We are counting the number of 0 here 
             if nums[right] == 0:
                 curr +=1
-            
+            #when the number or zero went from more than k than we are 
             while curr > k:
                 if nums[left] == 0:
                     curr -=1
