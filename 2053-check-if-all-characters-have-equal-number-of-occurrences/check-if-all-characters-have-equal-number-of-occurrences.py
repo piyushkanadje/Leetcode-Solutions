b22787fa0975c1cac6b4a50132c4ff399d07cb94
Piyush Prabhakar Kanadje <41730532+piyushkanadje@users.noreply.github.com>
2024-04-27 14:35:08 -0700

    Time: 35 ms (77.91%) | Memory: 16.9 MB (9.25%) - LeetSync

--- 2053-check-if-all-characters-have-equal-number-of-occurrences/check-if-all-characters-have-equal-number-of-occurrences.py
+++ 2053-check-if-all-characters-have-equal-number-of-occurrences/check-if-all-characters-have-equal-number-of-occurrences.py
@@ -1,7 +1,3 @@
 class Solution:
     def areOccurrencesEqual(self, s: str) -> bool:
-        count = collections.defaultdict(int)
-        for c in s:
-            count[c] +=1
-        
-        return len(set(count.values())) ==1 
+        return len(set(collections.Counter(s).values())) ==1 
