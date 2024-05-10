e15f1e80ff19edcd52b046949709d11d588f7dc6
Piyush Prabhakar Kanadje <41730532+piyushkanadje@users.noreply.github.com>
2024-05-10 13:29:31 -0700

    Time: 525 ms (13.95%) | Memory: 29.5 MB (84.29%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
@@ -1,27 +1,13 @@
-import random
-
 class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        heap = []
 
-    def partition(self, arr, low, high):
-        x = arr[high]
-        i = low
-        for j in range(low, high):
-            if arr[j] <= x:
-                arr[j], arr[i] = arr[i], arr[j]
-                i += 1
-        arr[i], arr[high] = arr[high], arr[i]
-        return i 
-
-    def kthSmallest(self, arr, low, high, k):
-        if k > 0 and k <= high - low + 1:
-            index = self.partition(arr, low, high)
-            if index - low == k - 1:
-                return arr[index]
-            if index - low > k - 1:
-                return self.kthSmallest(arr, low, index - 1, k)
-            return self.kthSmallest(arr, index + 1, high, k - index + low - 1)
-
-    def findKthLargest(self, nums, k):
-        return self.kthSmallest(nums, 0, len(nums) - 1, len(nums) - k + 1)
-
-
+        for i in range(len(nums)):
+            heapq.heappush(heap, nums[i] * -1)
+        
+        result = 0
+        while k > 0:
+            result = heapq.heappop(heap)
+            k -= 1
+        
+        return result * -1
