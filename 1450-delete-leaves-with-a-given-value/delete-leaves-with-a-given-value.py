090b0f72738e1513d1863633128a240ea19daa0a
Piyush Prabhakar Kanadje <41730532+piyushkanadje@users.noreply.github.com>
2024-06-19 19:44:40 -0700

    Time: 44 ms (65.86%) | Memory: 17.2 MB (26.21%) - LeetSync

--- 1450-delete-leaves-with-a-given-value/delete-leaves-with-a-given-value.py
+++ 1450-delete-leaves-with-a-given-value/delete-leaves-with-a-given-value.py
@@ -6,32 +6,16 @@
 #         self.right = right
 class Solution:
     def removeLeafNodes(self, root: Optional[TreeNode], target: int) -> Optional[TreeNode]:
-        if not root:
-            return root
-        if root.val == target and root.right is None and not root.left:
+        # base case
+        if not root: return None
+
+        # work - postorder traverse - need to handle children first before process parent in case parent needs 
+        # to be deleted after children deleted
+        root.left = self.removeLeafNodes(root.left, target)
+        root.right = self.removeLeafNodes(root.right, target)
+
+        # process root
+        if not root.left and not root.right and root.val == target:
             return None
-
-
-
-        def dfs(node, parent=None):
-            if not node:
-                return
-            
-            # Traverse the left subtree
-            dfs(node.left, node)
-            # Traverse the right subtree
-            dfs(node.right, node)
-
-            # Check if the current node is the leaf node to be deleted
-            if node.val == target and not node.left and not node.right:
-                print(f"Deleting leaf node with value: {node.val}")
-                if parent:
-                    if parent.left == node:
-                        parent.left = None
-                    elif parent.right == node:
-                        parent.right = None
-
-        dfs(root)
-        if root.val == target and not root.left and not root.right:
-            return None
+        
         return root
