78423661d99a619a6519245396cedfc82b8149ad
Piyush Prabhakar Kanadje <41730532+piyushkanadje@users.noreply.github.com>
2024-07-12 11:47:40 -0700

    Time: 40 ms (56.63%) | Memory: 17.7 MB (26.39%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
@@ -6,19 +6,17 @@
 #         self.right = right
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
+        
+
         if not root:
             return 0
-        stack = [(root,1)]
-        ans = 0
-
-        while stack:
-            node , depth = stack.pop()
-            ans = max(depth, ans)
-            if node.left:
-                stack.append((node.left, depth+1))
-            if node.right:
-                stack.append((node.right, depth+1))
-            
-        return ans 
         
-    
+        def dfs(root):
+            if not root:
+                return 0
+            #here we are writing left and right because we want to use this left and right to get the max of the left or right like in the current node right might be the logest path to maintain which is longest we are assigning the variables.
+            left = dfs(root.left)
+            right= dfs(root.right)
+            return max(left,right) + 1
+        
+        return dfs(root)
