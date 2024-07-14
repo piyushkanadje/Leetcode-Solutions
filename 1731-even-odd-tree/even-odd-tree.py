c62ba62dd63845f104964269f644f3f88a75c5ea
Piyush Prabhakar Kanadje <41730532+piyushkanadje@users.noreply.github.com>
2024-07-14 13:28:42 -0700

    Time: 247 ms (35.69%) | Memory: 37.7 MB (63.09%) - LeetSync

--- 1731-even-odd-tree/even-odd-tree.py
+++ 1731-even-odd-tree/even-odd-tree.py
@@ -5,56 +5,30 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def isIncreasing(self, arr):
-        if len(arr) == 1:
-            return arr[0] % 2 != 0  # Should be odd
-        for i in range(len(arr) - 1):
-            if arr[i] % 2 == 0 or arr[i + 1] % 2 == 0:  # Should be odd
-                return False
-            if arr[i] >= arr[i + 1]:  # Should be strictly increasing
-                return False
-        return True
-
-    def isDecreasing(self, arr):
-        if len(arr) == 1:
-            return arr[0] % 2 == 0  # Should be even
-        for i in range(len(arr) - 1):
-            if arr[i] % 2 != 0 or arr[i + 1] % 2 != 0:  # Should be even
-                return False
-            if arr[i] <= arr[i + 1]:  # Should be strictly decreasing
-                return False
-        return True
-
     def isEvenOddTree(self, root: Optional[TreeNode]) -> bool:
-        queue = []
-        levelArr = []
-        queue.append(root)
-        if root.val%2==0:
+        if not root:
             return False
+        queue=[]
+        queue.append(root)
+        level =0
         while queue:
-            arr = []
-            queuLen= len(queue)
-            for i in range(queuLen):
-                curr = queue.pop(0)
-                arr.append(curr.val)
-                if curr.left:
-                    queue.append(curr.left)
-                if curr.right:
-                    queue.append(curr.right)
+            queueLen= len(queue)
+            currn = float("-inf") if level %2 ==0 else float('inf')
 
-            levelArr.append(arr)
-            print(arr)
-        val=0
-        for i in range(len(levelArr)):
-            if i % 2==0:
-                val= self.isIncreasing(levelArr[i])
-                print(val)
-                if val == False:
-                    return val
-            else:
-                val= self.isDecreasing(levelArr[i])
-                if val == False:
-                    return val
-        return val
+            for _ in range(queueLen):
+                node = queue.pop(0)
+                if level %2 ==0:
+                    if node.val %2==0 or node.val <= currn:
+                        return False
+                if level %2 !=0 :
+                    if node.val %2 !=0 or node.val >= currn:
+                        return False
+                currn = node.val
 
-
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            level+=1
+        
+        return True
