b8958e88a60c10ceff5e56c587837440a8cf1eab
Piyush Prabhakar Kanadje <41730532+piyushkanadje@users.noreply.github.com>
2024-10-13 17:57:42 -0700

    Time: 581 ms (5.11%) | Memory: 31.2 MB (12.33%) - LeetSync

--- 1620-check-if-array-pairs-are-divisible-by-k/check-if-array-pairs-are-divisible-by-k.py
+++ 1620-check-if-array-pairs-are-divisible-by-k/check-if-array-pairs-are-divisible-by-k.py
@@ -1,17 +1,20 @@
 class Solution:
     def canArrange(self, arr: List[int], k: int) -> bool:
-        d = defaultdict()
+        d ={}
+
         for i in arr:
-            #we are doing i%k+k due to we want the value to be always positive and then doing %k because we want the value to be in range(0, k-1)
-            d[(i%k+k)%k]= d.get((i%k+k)%k,0) + 1
+                        #we are doing i%k+k due to we want the value to be always positive and then doing %k because we want the value to be in range(0, k-1)
+
+            d[(i%k+k)%k] = 1+ d.get((i%k+k)%k, 0) 
         
+        print(d)
         for i in arr:
             rem = (i%k+k)%k
-
+            #here we are checking all the numbers which are divisible by k
             if rem == 0:
                 if d[0] % 2==1:
                     return False
-            
+            #Here we are cheking the pairs in which k-rem means lets say i = 1 and k=5 k-rem will be 4 we will need 4 in d to make 1+4==5 
             elif d[rem] != d.get(k-rem, 0):
                 return False
         return True
