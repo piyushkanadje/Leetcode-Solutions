851ce8cadcebe9c87bbe8e316c8d0264ef1828e2
Piyush Prabhakar Kanadje <41730532+piyushkanadje@users.noreply.github.com>
2024-10-13 17:19:49 -0700

    Time: 42 ms (52.85%) | Memory: 16.6 MB (36.04%) - LeetSync

--- 1298-reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.py
+++ 1298-reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.py
@@ -1,18 +1,14 @@
 class Solution:
     def reverseParentheses(self, s: str) -> str:
-        open_parentheses_indices = deque()
+        stack = []
         result = []
-
-        for current_char in s:
-            if current_char == "(":
-                # Store the current length as the start index
-                # for future reversal
-                open_parentheses_indices.append(len(result))
-            elif current_char == ")":
-                start = open_parentheses_indices.pop()
-                # Reverse the substring between the matching parentheses
+        
+        for char in s:
+            if char == "(":
+                stack.append(len(result))
+            elif char == ")":
+                start = stack.pop()
                 result[start:] = result[start:][::-1]
             else:
-                # Append non-parenthesis characters to the processed list
-                result.append(current_char)
+                result.append(char)
         return "".join(result)
