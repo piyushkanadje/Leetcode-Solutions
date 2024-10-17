58f230a7c8f8214893d860849e55e252d9f48ec0
Piyush Prabhakar Kanadje <41730532+piyushkanadje@users.noreply.github.com>
2024-10-16 22:47:42 -0700

    Time: 567 ms (49.93%) | Memory: 31.7 MB (95.62%) - LeetSync

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.py
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.py
@@ -6,12 +6,14 @@ class Solution:
         s = set()
         while r < len(nums):
             while nums[r] in s:
+             
                 total -=nums[l]
                 s.remove(nums[l])
                 l+=1
+                
             total +=nums[r]
             s.add(nums[r])
-
+            
             if (r - l +1)==k:
                 ms= max(ms, total)
                 total -=nums[l]
