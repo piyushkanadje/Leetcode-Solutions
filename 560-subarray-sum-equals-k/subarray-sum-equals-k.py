b6fe6fa40cd4e4198513c999468c8539a36c5675
Piyush Prabhakar Kanadje <41730532+piyushkanadje@users.noreply.github.com>
2024-10-18 15:01:46 -0700

    Time: 34 ms (100.00%) | Memory: 21.1 MB (9.00%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,15 +1,11 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        currPrefixSum = 0
-        ans =0
-        count =collections.defaultdict(int)
-       # We need to initialize counts[0] = 1. This is because the empty prefix [] has a sum of 0. You'll see why this is necessary in a second.
-        count[0]  = 1
+        curr = 0
+        ans = 0
+        count = collections.defaultdict(int)
+        count[0]= 1
         for i in nums:
-            currPrefixSum +=i
-            #prefix sum j to i prefix_sum[j] - prefix_sum[i-1]
-            #here prefix_sum[j] - prefix_sum[i-1] = k
-            #so prefix_sum[j] - k = prefix_sum[i-1]  means there is subarray with sum k
-            ans +=count[currPrefixSum - k]
-            count[currPrefixSum] =1 + count.get(currPrefixSum, 0)
+            curr+=i
+            ans +=count[curr-k]
+            count[curr] =1  + count.get(curr, 0)
         return ans
