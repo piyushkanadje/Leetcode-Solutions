2386aeab5a9ae87bcf14688baca492a292ee5860
Piyush Prabhakar Kanadje <41730532+piyushkanadje@users.noreply.github.com>
2024-10-18 16:03:27 -0700

    Time: 100 ms (100.00%) | Memory: 22.9 MB (5.75%) - LeetSync

--- 525-contiguous-array/contiguous-array.py
+++ 525-contiguous-array/contiguous-array.py
@@ -1,17 +1,20 @@
 class Solution:
     def findMaxLength(self, nums: List[int]) -> int:
-        n=len(nums)
-        n1=0
-        n0=0
-        maxLen=0
-        mp={}
-        mp[0]=-1
-        for i in range(n):
-            n1+=nums[i]
-            n0=(i+1)-n1
-            if (n1-n0) in mp:
-                maxLen=max(maxLen, i-mp[n1-n0])
+        prefix_sum = {0:-1}
+        curr_sum = 0
+        max_len = 0
+        for i in range(len(nums)):
+
+            #The core of the problem is finding subarrays with equal numbers of 0s and 1s, which is equivalent to finding subarrays where the sum of the elements is zero if we convert the 0s to -1s.  
+            #Then, the problem becomes finding subarrays where the prefix sum is equal to zero, much like the problem you explained about finding subarrays with a sum equal to k.
+            if nums[i] == 0:
+                curr_sum -=1
             else:
-                mp[n1-n0]=i
-        return maxLen
+                curr_sum +=1
+            
+            if curr_sum in prefix_sum:
+                max_len= max(max_len, i - prefix_sum[curr_sum])
+            else:
+                prefix_sum[curr_sum] = i
         
+        return max_len
