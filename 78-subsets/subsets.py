8c3ea2cff60d81fbc8ae586e67b09e645c43998c
Piyush Prabhakar Kanadje <41730532+piyushkanadje@users.noreply.github.com>
2024-10-18 12:21:47 -0700

    Time: 145 ms (5.42%) | Memory: 17.7 MB (6.67%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -6,9 +6,12 @@ class Solution:
         def dfs(index):
             if index >=len(nums):
                 res.append(subsets.copy())
+                print(res)
                 return
+            #Decision to include the nums[i]
             subsets.append(nums[index])
-            dfs(index+ 1)
+            dfs(index+ 1)      
+            #Decision to exclude nums[o]
             subsets.pop()
             dfs(index+1)
         dfs(0)
