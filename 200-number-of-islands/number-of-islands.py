947b1f3d7cffe73f1b0159bf3d061b8518a65dda
Piyush Prabhakar Kanadje <41730532+piyushkanadje@users.noreply.github.com>
2024-10-21 22:49:22 -0700

    Time: 258 ms (40.95%) | Memory: 27.2 MB (14.87%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,38 +1,26 @@
-import collections
-
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid:
-            return 0
         
-        row , col = len(grid), len(grid[0])
+        def dfs(i, j):
+            for row, col in directions:
+                new_row , new_col = i+ row, j+col
+                if 0<= new_row < len(grid) and 0<= new_col < len(grid[0]) and grid[new_row][new_col] =="1" and (new_row, new_col) not in visited:
+                    visited.add((new_row, new_col))
+                    dfs(new_row, new_col)
+                    
+
+        directions = [[1,0],[0,1],[-1,0],[0,-1]]
         visited = set()
         island = 0
+        rows = len(grid)
+        cols = len(grid[0])
 
-        # def bfs(r,c):
-        #     queue = collections.deque()
-        #     visited.add((r,c))
-        #     queue.append((r,c))
-        #     while queue:
-        #         row, col = queue.popleft()
-        #         direction=[[1,0],[-1,0],[0,1],[0,-1]]
-        #         for dr, dc in direction:
-        #             new_r ,new_c =  row + dr, col + dc
-        #             if(new_r in range(row) and new_c in range(col) and grid[new_r][new_c] =="1" and (new_r,new_c) not in visited):
-        #                 queue.append((new_r,new_c))
-        #            visited.add((new_r,new_c))
-        def dfs(r,c):
-            if (r not in range(row)or c not in range(col) or grid[r][c]=="0"or (r,c) in visited):
-                return
-            visited.add((r,c))
-            directions = [[0,1],[0,-1],[1,0],[-1,0]]
-            for dr, dc in directions:
-                dfs(r+dr,c+dc)
-
-        for r in range(row):
-            for c in range(col):
-                if(grid[r][c] == "1" and (r,c) not in visited):
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j]=="1" and (i,j) not in visited:
                     island+=1
-                    dfs(r,c)
-                  
+                    visited.add((i,j))
+                    dfs(i,j)
+
         return island
+
