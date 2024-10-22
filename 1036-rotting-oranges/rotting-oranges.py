b91812b8d0eea10d2e24fd782cb02054fdecf7e5
Piyush Prabhakar Kanadje <41730532+piyushkanadje@users.noreply.github.com>
2024-10-21 23:37:20 -0700

    Time: 5 ms (97.78%) | Memory: 16.7 MB (30.14%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -7,6 +7,10 @@ class Solution:
         time = 0
         #Calculate the number or fresh and add rotton to the queue beaacuse while process the loop (while) we need to keep track of the fresh if fresh does not exist what to check then 
         #For each iteration we will increase time
+        # If, at the end of the process, some fresh oranges remain (fresh > 0), you return -1 because it means those fresh oranges cannot rot (i.e., they're isolated).
+
+
+#all the rotten oranges at once by adding them to the queue initially. This allows the BFS to spread the rot to neighboring oranges level by level (minute by minute). This is also known as a multi-source BFS,
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c]==1:
