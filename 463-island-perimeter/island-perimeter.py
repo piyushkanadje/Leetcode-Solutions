c4a2eb5c0cf423c99d718c9c6514ece2a96e1cbe
Piyush Prabhakar Kanadje <41730532+piyushkanadje@users.noreply.github.com>
2024-10-21 22:37:52 -0700

    Time: 155 ms (96.96%) | Memory: 25 MB (5.26%) - LeetSync

--- 463-island-perimeter/island-perimeter.py
+++ 463-island-perimeter/island-perimeter.py
@@ -1,25 +1,27 @@
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
-        rows  = len(grid)
-        cols = len(grid[0])
-        visit = set()
-        def dfs(l,r):
-
-            if(l >=rows or r >= cols or l < 0 or r < 0 or grid[l][r]==0):
+        perim = 0
+        visited = set()
+        rows = len(grid)
+        cols =len(grid[0])
+        def dfs(i, j):
+            if(i >=rows or j >= cols or i < 0 or j < 0 or grid[i][j]==0):
                 return 1
-            if (l,r) in visit:
+                return 1
+            if (i, j) in visited:
                 return 0
-
-            visit.add((l,r))
-            perim = dfs(l, r + 1)
-            perim += dfs(l + 1, r)
-            perim += dfs(l, r - 1)
-            perim += dfs(l - 1, r)
             
+            visited.add((i,j))
+            perim= dfs(i,j)
+            perim += dfs(i+1,j)
+            perim+= dfs(i, j+1)
+            perim+=dfs(i-1, j)
+            perim+=dfs(i,j-1)
+
             return perim
         
-
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j]:
-                    return dfs(i, j)
+                   return dfs(i,j)
+
