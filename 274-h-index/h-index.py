769c606e1a95a8d071b8f2d6493950b645cda014
Piyush Prabhakar Kanadje <41730532+piyushkanadje@users.noreply.github.com>
2024-10-25 20:04:31 -0700

    Time: 0 ms (100.00%) | Memory: 16.8 MB (68.67%) - LeetSync

--- 274-h-index/h-index.py
+++ 274-h-index/h-index.py
@@ -1,5 +1,8 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
+
+        #think like a geometry graph with on x axis number of paper and y axis number of citations
+        #Then the max number of citation==paper is answer
         n = len(citations)
         citations.sort()
 
