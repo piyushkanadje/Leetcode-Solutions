0a4dfbeef9c210aab2275b56df2de6202d5ac41e
Piyush Prabhakar Kanadje <41730532+piyushkanadje@users.noreply.github.com>
2024-11-05 11:16:27 -0800

    Time: 19 ms (77.03%) | Memory: 16.9 MB (89.02%) - LeetSync

--- 3174-minimum-number-of-changes-to-make-binary-string-beautiful/minimum-number-of-changes-to-make-binary-string-beautiful.py
+++ 3174-minimum-number-of-changes-to-make-binary-string-beautiful/minimum-number-of-changes-to-make-binary-string-beautiful.py
@@ -1,28 +1,21 @@
 class Solution:
     def minChanges(self, s: str) -> int:
-        # Initialize with first character
-        current_char = s[0]
-
-        consecutive_count = 0
         min_changes_required = 0
 
-        # Iterate through each character
-        for char in s:
-            # If current character matches the previous sequence
-            if char == current_char:
-                consecutive_count += 1
-                continue
-
-            # If we have even count of characters, start new sequence
-            if consecutive_count % 2 == 0:
-            
-                consecutive_count = 1
-            # If odd count, we need to change current character
-            else:
-                consecutive_count = 0
+        # Check pairs of characters (i, i+1) with step size 2
+        for i in range(0, len(s), 2):
+            # If characters in current pair don't match,
+            # we need one change to make them equal
+            if s[i] != s[i + 1]:
                 min_changes_required += 1
-
-            # Update current character for next iteration
-            current_char = char
-
         return min_changes_required
+
+
+"""
+pythonic one liner:
+
+class Solution:
+    def minChanges(self, s: str) -> int:
+        # Count changes needed for each unmatched pair
+        return sum(s[i] != s[i + 1] for i in range(0, len(s), 2))
+"""
