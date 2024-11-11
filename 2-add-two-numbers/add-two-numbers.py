71ea1a79f78776fb3b4698c9eca7a12457f01225
Piyush Prabhakar Kanadje <41730532+piyushkanadje@users.noreply.github.com>
2024-11-11 12:51:09 -0800

    Time: 4 ms (53.27%) | Memory: 16.8 MB (21.47%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -5,30 +5,36 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        dummy = ListNode(0)
+        dummy1 = l1
+        dummy2 = l2
         carry = 0
-        curr = dummy
-        while l1!=None or l2!=None or carry == 1:
-            sum = 0
-        #Here we are checking because we have different size linkedlist
-            if l1!=None:
-                sum += l1.val
-                l1 = l1.next
-            if l2!=None:
-                sum +=l2.val
-                l2= l2.next
-        
-        #we have to add carry in the 
-            sum+=carry
-        #Recalculate the carru
-            carry = sum//10
-            node = ListNode(sum%10)
-        #First curr was pointing to None (dummy)
-            curr.next = node
-        #here we want it to be node which will be helpful to have a new node
+        newlinked = ListNode()
+        curr = newlinked
+        while dummy1 and dummy2:
+            summ = dummy1.val + dummy2.val + carry
+            carry = summ //10
+            curr.next = ListNode(summ % 10)
             curr = curr.next
-        
-        return dummy.next
+            dummy1 = dummy1.next
+            dummy2 = dummy2.next
 
-        
-        
+        while dummy1:
+            summ = dummy1.val + carry
+
+            carry = summ //10
+            curr.next = ListNode(summ % 10)
+            curr = curr.next
+            dummy1 = dummy1.next
+
+        while dummy2:
+            summ = dummy2.val + carry
+            carry = summ //10
+            curr.next = ListNode(summ % 10)
+            curr = curr.next
+            dummy2 = dummy2.next
+            
+
+        if carry > 0:
+            curr.next = ListNode(carry)
+       
+        return newlinked.next
