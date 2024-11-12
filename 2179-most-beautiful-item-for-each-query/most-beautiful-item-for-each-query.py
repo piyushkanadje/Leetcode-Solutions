d945de6f613054b26451c310f4df02aeb03d9ca0
Piyush Prabhakar Kanadje <41730532+piyushkanadje@users.noreply.github.com>
2024-11-12 15:19:43 -0800

    Time: 156 ms (60.38%) | Memory: 65.9 MB (60.77%) - LeetSync

--- 2179-most-beautiful-item-for-each-query/most-beautiful-item-for-each-query.py
+++ 2179-most-beautiful-item-for-each-query/most-beautiful-item-for-each-query.py
@@ -1,25 +1,19 @@
 class Solution:
-    def maximumBeauty(self, items, queries):
+    def maximumBeauty(self, items: List[List[int]], queries: List[int]) -> List[int]:
+        sorted_items= sorted(items, key = lambda x: x[0])
         ans = [0] * len(queries)
 
-        # sort both items and queries in ascending order
-        items.sort(key=lambda x: x[0])
-
-        queries_with_indices = [[queries[i], i] for i in range(len(queries))]
-
-        queries_with_indices.sort(key=lambda x: x[0])
-
+        queries_with_index = [[queries[i],i] for i in range(len(queries))]
+        queries_with_index.sort(key= lambda x:x[0])
+        maxxBeauty = 0
         item_index = 0
-        max_beauty = 0
+        for i in range(len(queries_with_index)):
 
-        for i in range(len(queries)):
-            query = queries_with_indices[i][0]
-            original_index = queries_with_indices[i][1]
+            while item_index < len(sorted_items) and sorted_items[item_index][0] <= queries_with_index[i][0]:
+                maxxBeauty = max(maxxBeauty, sorted_items[item_index][1])
+                item_index+=1
 
-            while item_index < len(items) and items[item_index][0] <= query:
-                max_beauty = max(max_beauty, items[item_index][1])
-                item_index += 1
-
-            ans[original_index] = max_beauty
-
+            ans[queries_with_index[i][1]] = maxxBeauty
         return ans
+
+
