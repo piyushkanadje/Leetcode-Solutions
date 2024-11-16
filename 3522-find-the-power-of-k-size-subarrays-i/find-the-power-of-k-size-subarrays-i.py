30ace3f313a6c1e17f53e209dd8ae26628136a49
Piyush Prabhakar Kanadje <41730532+piyushkanadje@users.noreply.github.com>
2024-11-16 13:42:43 -0800

    Time: 5 ms (63.16%) | Memory: 17 MB (6.10%) - LeetSync

--- 3522-find-the-power-of-k-size-subarrays-i/find-the-power-of-k-size-subarrays-i.py
+++ 3522-find-the-power-of-k-size-subarrays-i/find-the-power-of-k-size-subarrays-i.py
@@ -1,21 +1,14 @@
 class Solution:
     def resultsArray(self, nums: List[int], k: int) -> List[int]:
-        length = len(nums)
-        result = [-1]* (length - k + 1)
-        index_deque = collections.deque()
-
-        for curr_index in range(length):
-            if index_deque and index_deque[0] < curr_index -k +1:
-                index_deque.popleft()
-            if index_deque and nums[curr_index] != nums[curr_index -1]+1:
-                index_deque.clear()
-            
-            index_deque.append(curr_index)
-
-            if curr_index >=k-1:
-                if len(index_deque) == k:
-                    result[curr_index - k +1] = nums[index_deque[-1]]
-                else:
-                    result[curr_index - k + 1] = -1
-            
-        return result
+        consec_count = 1
+        res = []
+        for i in range(len(nums)):
+            if i and nums[i - 1] + 1 == nums[i]:
+                consec_count += 1
+            else:
+                consec_count = 1
+            if consec_count >= k:
+                res.append(nums[i])
+            elif i >= k - 1:
+                res.append(-1)
+        return res
