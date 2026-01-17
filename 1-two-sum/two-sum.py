56e42a0903d86667e6336e3fc819ffa422dfb00d
Piyush Prabhakar Kanadje <41730532+piyushkanadje@users.noreply.github.com>
2026-01-16 21:28:04 -0600

    Time: 0 ms (100.00%) | Memory: 20.5 MB (11.67%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,10 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        d = {}
-    #  a + b (target) we will look for target-nums[i] if present take their indices Where to check indices in dictionary
+        d   = {}
         for i in range(len(nums)):
-            if (target-nums[i]) in d:
-                return [i, d[target-nums[i]]]
+            if target- nums[i] in d:
+                return [i , d[target- nums[i]]] 
             d[nums[i]] = i
-
-        return []
+        return -1
