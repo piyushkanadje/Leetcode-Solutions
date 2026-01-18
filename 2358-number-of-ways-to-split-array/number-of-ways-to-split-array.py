9ce990c46c3f36f9ed770133d43a84219691f3f5
Piyush Prabhakar Kanadje <41730532+piyushkanadje@users.noreply.github.com>
2026-01-18 16:51:13 -0600

    Time: 115 ms (14.02%) | Memory: 33.4 MB (27.11%) - LeetSync

--- 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.py
+++ 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.py
@@ -5,10 +5,7 @@ class Solution:
         for i in range(1, len(nums)):
             prefix.append(nums[i]+ prefix[i-1])
         for i in range(len(prefix)-1):
-            print(prefix[i])
-            print(prefix[len(nums)-1] - prefix[i])
             if prefix[i] >= (prefix[len(nums)-1] - prefix[i]):
-                print("Hello", i)
                 ans+=1
         return ans
         
