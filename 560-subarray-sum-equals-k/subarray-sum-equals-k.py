631768a9a284d2de3bf37bcce872c26d02ed1415
Piyush Prabhakar Kanadje <41730532+piyushkanadje@users.noreply.github.com>
2026-01-20 16:28:20 -0600

    Time: 31 ms (62.13%) | Memory: 22.1 MB (10.63%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,11 +1,27 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        curr = 0
         ans = 0
-        count = collections.defaultdict(int)
-        count[0]= 1
-        for i in nums:
-            curr+=i
-            ans +=count[curr-k]
-            count[curr] =1  + count.get(curr, 0)
+        # I tried this problem by using brute force then problem comes to n2 then with prefix s
+        #sum only the problem is again n2 same thing to calculate the subaary with the sam sum
+
+        #With hashmap, the idea is:
+        # If there exists an index i such that
+        # prefixSum[j] = prefixSum[i] + k
+        # (i.e., prefixSum[j] - prefixSum[i] = k),
+        # then the subarray from (i + 1) to j has sum k.
+
+        #if the prefixSum[j] - prefixSum[i]  = 0 means they are equal that means the sum between 2 is 0
+        #so here we store the prefixSum of each time and check and to get our ans currSum - k and check if this presents or not if this presents means we can remove this for this much times to get he required sum
+
+        currSum = 0
+        hashMap = {0:1}
+        ans = 0
+        for i in range(len(nums)): 
+            currSum += nums[i]
+            if currSum - k in hashMap:
+                ans +=hashMap[currSum-k]
+            hashMap[currSum] = 1 + hashMap.get(currSum,0)
+
         return ans
+            
+
