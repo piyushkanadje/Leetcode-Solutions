f13176f946b39759c0fe1ac4b04f2dcdc5e3ade4
Piyush Prabhakar Kanadje <41730532+piyushkanadje@users.noreply.github.com>
2026-01-29 16:10:39 -0600

    Time: 30 ms (68.74%) | Memory: 21.8 MB (43.19%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -13,6 +13,9 @@ class Solution:
         #if the prefixSum[j] - prefixSum[i]  = 0 means they are equal that means the sum between 2 is 0
         #so here we store the prefixSum of each time and check and to get our ans currSum - k and check if this presents or not if this presents means we can remove this for this much times to get he required sum
 
+        #here we are adding zero because lets say if we get curr-k = 0 and we do not have a subarray which is same then we would lost the valid subarray
+
+
         currSum = 0
         hashMap = {0:1}
         ans = 0
