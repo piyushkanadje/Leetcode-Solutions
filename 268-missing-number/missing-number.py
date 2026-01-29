74ef2087fea0b15ef76c6a67313ac91818d9d3bd
Piyush Prabhakar Kanadje <41730532+piyushkanadje@users.noreply.github.com>
2026-01-29 08:13:47 -0600

    Time: 0 ms (100.00%) | Memory: 20.6 MB (17.67%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -1,6 +1,6 @@
 class Solution:
-    def sum_of_n(self, n):
-        return n * (n + 1) // 2
     def missingNumber(self, nums: List[int]) -> int:
+        summ = sum(nums)
+        n = len(nums)
         
-        return self.sum_of_n(len(nums)) - sum(nums)
+        return  (n*(n+1)//2) - summ
