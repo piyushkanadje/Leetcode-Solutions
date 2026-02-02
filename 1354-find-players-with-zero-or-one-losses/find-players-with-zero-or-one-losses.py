87e5a0398614beaecd54238139dafd846da73d13
Piyush Prabhakar Kanadje <41730532+piyushkanadje@users.noreply.github.com>
2026-02-01 18:00:40 -0600

    Time: 140 ms (38.33%) | Memory: 60.4 MB (68.65%) - LeetSync

--- 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.py
+++ 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.py
@@ -1,26 +1,21 @@
 class Solution:
-    def findWinners(self, matches: List[List[int]]) -> List[List[int]]:
+    def findWinners(self, matches: List[List[int]]) -> List[List[int]]: 
+        d = collections.defaultdict(int)
+        for i , j in matches:
+            d[i]+=0
+            d[j]+=1
+        
+        zeroLost = []
+        oneLost = []
+        for i in d.keys():
+            if d[i]==0:
+                zeroLost.append(i)
+            if d[i] ==1:
+                oneLost.append(i)
 
-        d = {}
-        for match in matches:
-            d[match[1]] = d.get(match[1], 0) + 1
-            d[match[0]] = d.get(match[0], 0)
-        #print(dlost)
-        # for i in matches:
-        #     if (i[0] not in dlost) and (i[0] not in notlost):
-        #         notlost.append(i[0])
-        #     if (i[0] in dlost) and (dlost[i[0]]==1) and (i[0] not in lost1) :
-        #         lost1.append(i[0])
-        #     if dlost[i[1]] == 1 and i[1] not in lost1:
-        #         lost1.append(i[1])
-        ans1 = []
-        ans2 = []
+        return [sorted(zeroLost),sorted(oneLost)]
+        
 
-        for key, val in d.items():
-            if val == 0:
-                ans1.append(key)
-            if val == 1:
-                ans2.append(key)
-        ans1.sort()
-        ans2.sort()
-        return[ans1, ans2]
+
+
+
