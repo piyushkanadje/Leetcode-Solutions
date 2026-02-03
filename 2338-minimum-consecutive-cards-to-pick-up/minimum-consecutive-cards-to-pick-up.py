49de74a944dac7fb62e369066d12caffa30d1191
Piyush Prabhakar Kanadje <41730532+piyushkanadje@users.noreply.github.com>
2026-02-03 16:48:26 -0600

    Time: 75 ms (58.20%) | Memory: 37.5 MB (65.08%) - LeetSync

--- 2338-minimum-consecutive-cards-to-pick-up/minimum-consecutive-cards-to-pick-up.py
+++ 2338-minimum-consecutive-cards-to-pick-up/minimum-consecutive-cards-to-pick-up.py
@@ -1,18 +1,11 @@
 class Solution:
     def minimumCardPickup(self, cards: List[int]) -> int:
-        left = 0
-        ans = float('inf')
-        card_index = collections.defaultdict(int)
-        for right in range(len(cards)):
-            if cards[right] in card_index:
-                # Calculate the window size only if this card has been seen before
-                window_size = right - card_index[cards[right]] + 1
-                ans = min(ans, window_size)
-                # If the smallest possible answer is found, return early
-                if ans == 2:
-                    return ans
-            # Update the last index of the current card
-            card_index[cards[right]] = right
-
-        # If no such subarray is found, return -1
-        return -1 if ans == float('inf') else ans
+        d = {}
+        ans = float("inf")
+        for  i in range(len(cards)):
+            if cards[i]  not in d:
+                d[cards[i]] = i
+            else:
+                ans = min(ans, (i- d[cards[i]]))
+                d[cards[i]] = i
+        return ans+1 if ans != float("inf") else -1
