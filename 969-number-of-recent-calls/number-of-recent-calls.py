413c44cd214cbea9453e81e62b28900424b60a3b
Piyush Prabhakar Kanadje <41730532+piyushkanadje@users.noreply.github.com>
2026-02-03 16:25:26 -0600

    Time: 32 ms (77.36%) | Memory: 24.7 MB (25.95%) - LeetSync

--- 969-number-of-recent-calls/number-of-recent-calls.py
+++ 969-number-of-recent-calls/number-of-recent-calls.py
@@ -1,15 +1,15 @@
-class RecentCounter:
+from collections import deque
 
+class RecentCounter:
     def __init__(self):
-        self.queue= collections.deque()
+        self.queue = deque()
 
     def ping(self, t: int) -> int:
-        while self.queue and self.queue[0] < t-3000:
+        while self.queue and self.queue[0] < t - 3000:
             self.queue.popleft()
         
         self.queue.append(t)
         return len(self.queue)
-        
 
 
 # Your RecentCounter object will be instantiated and called as such:
